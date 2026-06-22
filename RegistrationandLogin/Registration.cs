aab2342896fc00ad51cf7f77ac459be2da7b522c
Dedan M <88259619+DedanMugambiGikunda@users.noreply.github.com>
2026-06-22 10:38:54 +0300

    Add files via upload

---
+++ RegistrationandLogin/Registration.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegistrationandLogin
+{
+    public partial class Registration : Form
+    {
+        SqlCommand cmd;
+        SqlConnection cn;
+        SqlDataReader dr;
+        
+public Registration()
+        {
+            InitializeComponent();
+        }
+        private void Registration_Load(object sender, EventArgs e)
+        {
+            cn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Administrator\Desktop\RegandLog\RegistrationandLogin\Database.mdf;Integrated Security=True");
+            cn.Open();
+        }
+       
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (textBox3.Text != string.Empty || textBox2.Text != string.Empty ||
+            textBox1.Text != string.Empty)
+            {
+                if (textBox2.Text == textBox3.Text)
+                {
+                    cmd = new SqlCommand("select * from LoginTable where username='" +
+                    textBox1.Text + "'", cn);
+                    dr = cmd.ExecuteReader();
+                    if (dr.Read())
+                    {
+                        dr.Close();
+                        MessageBox.Show("Username Already exist please try another ", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        dr.Close();
+                        cmd = new SqlCommand("insert into LoginTable (username, password) values (@username, @password)", cn);
+
+                        cmd.Parameters.AddWithValue("username", textBox1.Text);
+                        cmd.Parameters.AddWithValue("password", textBox2.Text);
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Your Account is created . Please login now.", "Done",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please enter both password same ", "Error", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter value in all field.", "Error", MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Login login = new Login();
+            login.ShowDialog();
+        }
+    }
+}
